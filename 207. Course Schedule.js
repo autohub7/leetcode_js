60c892b1ff1a21bb359334d34fa256ad546fef6f
Steven Jin <vlsijin@gmail.com>
2018-03-29 09:52:25 -0700

    Update 207. Course Schedule.js

--- 207. Course Schedule.js
+++ 207. Course Schedule.js
@@ -28,34 +28,3 @@ var canFinish = function(numCourses, prerequisites) {
 }
 
 console.log(canFinish(2, [[1,0]] ) )
-
-/*
-public class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int[] indegree = new int[numCourses];
-        Queue<Integer> queue = new LinkedList<Integer>();
-        for(int[] pair:prerequisites){
-            indegree[pair[1]]++;
-        }
-        for(int i=0;i<indegree.length;i++){
-            if(indegree[i]==0){
-                queue.add(i);
-            }
-        }
-        int select = 0;
-        while(!queue.isEmpty()){
-            numCourses--;
-            int course = queue.poll();
-            for(int[] pair:prerequisites){
-                if(pair[0]==course){
-                    indegree[pair[1]]--;
-                    if(indegree[pair[1]]==0){
-                        queue.add(pair[1]);
-                    }
-                }
-            }
-        }
-        return numCourses==0;
-    }
-}
-*/
